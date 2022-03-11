5e39ef244833214e66de5a67537a8e09ee40d3bc
abdul_bawa <aribzamayor@gmail.com>
2022-03-11 15:05:56 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-  puts("Programming is like building a ultilingual puzzle\n");
-  return(0);
+puts("Programming is like building a multilingual puzzle\n");
+return(0);
 }
