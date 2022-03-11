db4480eea545424e58def51e930749d9830e61f6
abdul_bawa <aribzamayor@gmail.com>
2022-03-11 16:19:02 +0000

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s) \n", sizeof(char));
+printf("Size of int: %d byte(s) \n", sizeof(int));
+printf("Size of long int: %d byte(s) \n", sizeof(long int));
+printf("Size of long long int: %d byte(s) \n", sizeof(long long int));
+printf("Size of float: %d byte(s) \n", sizeof(float));
+return (0);
+}
