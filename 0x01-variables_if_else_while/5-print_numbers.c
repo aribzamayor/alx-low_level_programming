35c6e50c407f70cc492392877d9bc902250b90c7
abdul_bawa <aribzamayor@gmail.com>
2022-03-14 13:12:40 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lowercase, and then in uppercase,
+*followed by a new line
+*Return: Always 0 (Success)
+**/
+int main(void)
+{
+int n = 0;
+while (n <= 10)
+{
+printf("%d\n", n);
+n++;
+}
+}
