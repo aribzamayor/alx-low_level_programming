5b0af2e12e6a489e1ebe8e78697a337e819787b1
abdul_bawa <aribzamayor@gmail.com>
2022-03-14 14:03:18 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lowercase, and then in uppercase,
+*followed by a new line
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
