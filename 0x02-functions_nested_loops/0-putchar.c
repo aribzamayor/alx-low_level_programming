9ff39d270c73a68a89d04d46c0d17c7b21c3d458
abdul_bawa <aribzamayor@gmail.com>
2022-03-15 15:37:04 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "putchar.h"
 /**
-* main - prints all possible combinations of two two-digit numbers
-* Return: Always 0 (Success)
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
