2a2e349c547f3dc85ab7b9f84879ef70b6786a79
abdul_bawa <aribzamayor@gmail.com>
2022-03-27 11:51:20 +0000

    task 1

--- 0x06-pointers_arrays_strings/1-strncat2.c
+++ 0x06-pointers_arrays_strings/1-strncat2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "string.h"
 /**
- * _strncat - concatenates n bytes from a string to another
+ *  * _strncat - concatenates n bytes from a string to another
  * @dest: destination string
  * @src: source string
  * @n: number of bytes of str to concatenate
