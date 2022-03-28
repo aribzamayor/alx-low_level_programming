78d8bdec11ec73dd3230ce01a3bafc3aa1509bf9
abdul_bawa <aribzamayor@gmail.com>
2022-03-28 11:49:15 +0000

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
-
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
 void reverse_array(int *a, int n)
 {
 int i;
