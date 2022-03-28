53bf9e987af62875ab879d456b7e3cf5af2d4325
abdul_bawa <aribzamayor@gmail.com>
2022-03-28 11:47:45 +0000

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+
+void reverse_array(int *a, int n)
+{
+int i;
+
+i = n - 1;
+while (i >= 0)
+{
+if (i != n - 1)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i--;
+}
+printf("\n");
+}
