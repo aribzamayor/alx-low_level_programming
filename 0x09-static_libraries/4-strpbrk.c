37e96e3a2708c80d08895411a5cf12ea21547fc9
abdul_bawa <aribzamayor@gmail.com>
2022-04-07 17:36:01 +0000

    task 0

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: stringcontaining the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *ans = strpbrk(s, accept);
+
+	return (ans);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ans = strstr(haystack, needle);
+
+	return (ans);
+}
