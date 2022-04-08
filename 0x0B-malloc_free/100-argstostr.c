7be929c680f5e19388ed492701dd1d68c01594ff
abdul_bawa <aribzamayor@gmail.com>
2022-04-08 12:44:01 +0000

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * *argstostr - concatenates all the arguments of the program
  * @ac: number of arguments
@@ -14,6 +15,7 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -21,18 +23,23 @@ char *argstostr(int ac, char **av)
 		len++;
 	}
 	str = malloc(sizeof(char) * (len + 1));
+
 	if (str == NULL)
 		return (NULL);
+
 	k = 0;
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
+
 			str[k] = av[i][j];
 			k++;
 		}
 		str[k] = '\n';
 		k++;
 	}
+
 	return (str);
 }
